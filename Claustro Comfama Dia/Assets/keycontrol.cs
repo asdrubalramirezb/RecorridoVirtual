2a73b39d92b7be9fa57072436942df16dc3db692
asdrubalramirezb <asdrubalramirezb@users.noreply.github.com>
2020-05-03 16:38:21 -0500

    New Project

---
+++ Claustro Comfama Dia/Assets/keycontrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keycontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject animations;
+    void Start()
+    {
+        animations.SetActive(false);
+        GetComponent<AudioSource>().time = GetComponent<AudioSource>().clip.length * .5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+     {
+          animations.SetActive(true);
+     }
+      else if (Input.GetKeyDown("escape") )
+      {
+        animations.SetActive(false);
+      }
+    }
+}
+
+
